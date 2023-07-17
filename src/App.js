95d4e90c1276201560b7466b6a91572a76180589
Richard Mac <78776822+richardmac604@users.noreply.github.com>
2023-07-17 12:49:03 -0700

    first commit - added base layout to the portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar.jsx";
+import Skills from "./components/Skills.jsx";
+import Work from "./components/Work.jsx";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
     </div>
   );
 }
---
+++ src/Data/data.js
@@ -0,0 +1,55 @@
+export const data = [ 
+    {
+        id:1,
+        name:"Nature Go",
+        //image:WorkIm,
+        github:"github.com/kialoenn/COMP-2800-Team-BBY-25-NatureGO",
+
+    },
+    {
+        id:2,
+        name:"Inter Explore",
+        //image:RealEst,
+        github:"github.com/richardmac604/inter-explore",
+
+    },
+    {
+        id:3,
+        name:"RDNKN",
+        //image:WorkIm,
+        github:"rdnkn.ca",
+
+    },
+    {
+        id:4,
+        name:"PUGMA ULTIMA",
+        //image:RealEst,
+        github:"github.com/richardmac604/PUGNA_ULTIMA",
+    },
+    {
+        id:4,
+        name:"PUGMA ULTIMA",
+        //image:RealEst,
+        github:"github.com/richardmac604/PUGNA_ULTIMA",
+    },
+    {
+        id:5,
+        name:"Fit Wo",
+        //image:RealEst,
+        github:"github.com/winter2022-comp3717/project-fitwo",
+    },
+    {
+        id:6,
+        name:"Sleepless Knights",
+        //image:RealEst,
+        github:"github.com/Sorumoru/sleepless-knights",
+    },
+    {
+        id:7,
+        name:"PUGMA ULTIMA",
+        //image:RealEst,
+        github:"github.com/richardmac604/PUGNA_ULTIMA",
+    },
+
+
+]
